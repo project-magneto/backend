df5a2d584dcf0d25ef0a251b6e171798b4e32c37
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-03-02 11:27:16 -0500

    feat: cors implemented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config();
 
 import {
@@ -11,6 +12,7 @@ import {
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
